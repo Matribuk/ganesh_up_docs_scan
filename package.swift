c34af5f1f4f6f3c87c638228b2f71e64b37b88af
Matribuk <antonin.leprest@epitech.eu>
2023-10-03 14:09:15 +0200

    fix(lib): package for lib external
    Signed-off-by: Matribuk <antonin.leprest@epitech.eu>

--- package.swift
+++ package.swift
@@ -1,11 +1,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyFlutterPlugin",
+    name: "WeScan",
     dependencies: [
         .package(url: "https://github.com/Matribuk/ganesh_up_wescan.git"),
     ],
     targets: [
-        .target(name: "MyFlutterPlugin", dependencies: ["WeScan"]),
+        .target(name: "WeScan", dependencies: ["WeScan"]),
     ]
 )
